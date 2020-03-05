ba567cf0f1802adc72fec44c281656779afc7bf4
aditya10185 <aditya10185@csu.fullerton.edu>
2020-03-05 16:58:09 -0600

    Initial commit

---
+++ src/com/codewithadi/Main.java
@@ -0,0 +1,13 @@
+package com.codewithadi;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+    String message="My name is \"Aditya\"";
+        System.out.println(message);
+
+    }
+}
